09a787e0fb56c98850e5383e90956fba829f6806
Daniel Käs <danielkaes@duck.com>
2024-01-06 17:36:57 +0100

    doc: adds more source code comments

--- src/server.c
+++ src/server.c
@@ -10,17 +10,27 @@
 #include <pthread.h>
 #include <unistd.h>
 
+/// @brief Default error handler of the service.
+///        This error handler will exit the program with the given error message.
+///        The handler can be changed by setting Server.error_cb to another function.
+/// @param msg Error message, must be a null terminated string
 static void fatal_error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
+/// @brief Callback for receiving a signal (default: SIGINT) to quit the server.
+/// @param signo Signal which was received.
 static void server_exit(int signo) {
     printf("server received shutdown signal %d\r\n", signo);
     exit(0);
 }
 
+/// @brief Accepts new incoming requests
+///        and calls the client callback of the server for each new connection.
+///        This functions is executing by each thread in the thread pool.
+/// @param arg pointer to the Server instance running the thread pool
 static void *handle_request_loop(void *arg)
 {
     Server *server = arg;
@@ -44,6 +54,9 @@ static void *handle_request_loop(void *arg)
     return 0;
 }
 
+/// @brief Bind to the given port and starts listening for new connections
+/// @param port port number to listen on
+/// @return socket file descriptor of the listening socket.
 static int setup_listening_socket(int port) {
     int sock;
     struct sockaddr_in srv_addr;
@@ -74,9 +87,9 @@ static int setup_listening_socket(int port) {
     if (listen(sock, 10) < 0)
         fatal_error("listen()");
 
-    return (sock);
+    return sock;
 }
-
+ 
 int server_init(Server *server, void (*client_cb)(int client_socket)) {
     if (!client_cb) {
         return EXIT_FAILURE;
